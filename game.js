7641011b8ff9d66160f6ec18030ab9aac057f83f
Wessel van de Weg <111873@imac-b42-14.local>
2019-05-20 11:20:31 +0200

    curbs gemaakt

--- game.js
+++ game.js
@@ -4,22 +4,38 @@ function setup() {
 }
 
 function draw() {
+
+    // background
     background("gray");
+    
+    // curbs zijkanten
+    var curbYWhite = 0;
+    var curbYRed = 40;
+
+    // Witte curbs
+    while (curbYWhite < 800) {
+        fill("white");
+        rect(0,curbYWhite,40,40);
+        rect(560,curbYWhite,40,40);
+
+        curbYWhite = curbYWhite + 80;
+    };
+
+    // Rode curbs
+    while (curbYRed < 800) {
+        fill("red");
+        rect(0,curbYRed,40,40);
+        rect(560,curbYRed,40,40);
+
+        curbYRed = curbYRed + 80;
+    };
+
     fill("blue");
     rect(mouseX, 600, 100, 170);
-
+    
     var beweging = 0;
-
+    
     fill("black");
     rect(200, beweging, 80, 120);
 
-    fill("white");
-    rect(0,0,40,40);
-
-    fill("red");
-    rect(0,40,40,40);
-    
-
-
-    
-}
+};
